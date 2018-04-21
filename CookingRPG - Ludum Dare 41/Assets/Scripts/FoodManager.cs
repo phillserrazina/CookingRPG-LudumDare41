175251a66230e4005da21216c8be6f6c57ac2eef
phillserrazina <fserrazina99@gmail.com>
2018-04-21 02:34:14 +0100

    First Commit

---
+++ CookingRPG - Ludum Dare 41/Assets/Scripts/FoodManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodManager : MonoBehaviour {
+
+	public string[] Ingredients = { "Rice", "Pasta", "Vegetables", "Spices", "Meat", "Fish", "Eggs", "Salt", "Fruit", "Sugar", "Chocolate", "Flower" };
+}
